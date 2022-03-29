718b0dd062a3c0b9cda56bc09794e126465dbb30
Aurimas <aurimas.gaidys@gmail.com>
2022-03-29 12:34:22 +0300

    Login updated

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,4 @@
+import { signIn, useSession } from 'next-auth/react';
 import Head from 'next/head'
 import { useRecoilState, useRecoilValue } from 'recoil';
 import { teamState } from '../atoms/userData';
@@ -8,7 +9,18 @@ import Modal from '../components/Modal'
 
 export default function Home() {
 
+  const { data: session, status } = useSession();
   const team = useRecoilValue(teamState);
+
+  if (session == null) {
+    <div className="">
+      <Head>
+        <title>Create test App</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => signIn()}>Sign in</button>
+    </div>
+  }
 
   return (
     <div className="">
