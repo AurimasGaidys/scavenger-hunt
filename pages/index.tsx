12b0ee51c8bdb298d3489abd9797227f1b0b6100
Aurimas <aurimas.gaidys@gmail.com>
2022-03-29 12:53:26 +0300

    Updated 1 page

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,11 +13,13 @@ export default function Home() {
   const team = useRecoilValue(teamState);
 
   if (session == null) {
-    return (<div className="">
+    return (<div className="flex justify-center p-8 flex-col">
       <Head>
         <title>Create test App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+      <h1 className='text-lg text-center p-4'>Great Treasure hunt</h1>
+      <h1 className='text-m text-center p-4'>You are almost there press sign in </h1>
       <button className="p-3 bg-blue-500 rounded-lg text-white" onClick={() => signIn()}>Sign in</button>
     </div>);
   }
