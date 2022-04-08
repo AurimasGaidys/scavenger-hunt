7b042af5a8d6e58d33c81d361d04af86ca1b351b
Aurimas <aurimas@cpmstars.com>
2022-04-08 11:46:01 +0300

    Game state fixed

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -18,7 +18,7 @@ function Profile() {
     return <div >
         <BonusHeader name="Profile" />
         <div className="p-4">
-            <h1 className="text-xl p-4">Profile</h1>
+            <h1 className="text-xl p-2">Profile</h1>
             {
                 !session ? <>
                     <h2 className="p-2">Logged out</h2>
@@ -26,17 +26,19 @@ function Profile() {
                 </>
                     :
                     <>
-                        <h2>Team mates</h2>
-                        <div className="flex flex-col space-y-2">{team?.membersIds?.map(x => <p key={"tm-" + x}>{x}</p>)} </div>
-                        <h2 className="text-xl p-4">Total Bonuses solved</h2>
-                        <div className="flex">{team?.solvedBonuses?.length} </div>
+                        <h2 className="text-xl p-2">Team mates</h2>
+                        <div className="flex flex-col space-y-2 p-4">{team?.membersIds?.map(x => <p key={"tm-" + x}>{x}</p>)} </div>
+                        <h2 className="text-xl p-2">Total Bonuses solved</h2>
+                        <div className="text-m p-4">{team?.solvedBonuses?.length} </div>
+                        <div className="flex"> {(team?.solvedBonuses?.length || 0)*60} sec bonus</div>
                         <h2 className="text-xl p-4">Game started</h2>
-                        <div className="flex">{moment(team?.gameStarted)}</div>
+                        {(team?.gameStarted || 0) > 0 &&<div className="text-m p-4">{moment(team?.gameStarted).format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>}
                         <h2 className="text-xl p-4">Game ended</h2>
-                        <div className="flex">{moment(team?.gameEnded)}</div>
+                        {(team?.gameEnded || 0) > 0 &&<div className="text-m p-4">{moment(team?.gameEnded).format("dddd, MMMM Do YYYY, h:mm:ss a")}</div>}
                         <h2 className="text-xl p-4">Game duration</h2>
-                        <div className="flex">{moment.duration(moment(team?.gameStarted).diff(moment(team?.gameEnded))).asMinutes()}</div>
-                        <h2 className="text-xl p-4">Logged out </h2>
+                        {(team?.gameEnded || 0) > 0 && <div className="text-m p-4">{moment.duration(moment(team?.gameEnded).diff(moment(team?.gameStarted))).asMinutes()}</div>}
+                        <h2 className="text-xl p-4">Total game time</h2>
+                        {(team?.gameEnded || 0) > 0 && <div className="text-m p-4">{moment.duration(moment(team?.gameEnded).diff(moment(team?.gameStarted))).asMinutes() - (team?.solvedBonuses?.length || 0)*60}</div>}
                         <button className="border p-2" onClick={() => { signOut() }}>Logout</button>
                     </>
             }
